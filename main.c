db77edc51fe130839910b9dc094e76c23e581490
J00J3X <jonathan.krefta040@gmail.com>
2025-05-11 00:58:54 -0300

    Atualização do main pra ficar mais didático

--- main.c
+++ main.c
@@ -4,7 +4,22 @@
 int main()
 {
     char inf[50] = "(A+B*(C-D*(E-F)-G*H)-I*3)";
+    char aux[50];
+
+    aux[50] = inf;
+
     traduzPosfixa(inf);
+
+    printf("Apresentando o programa!\n");
+    printf("Enter para continuar...");
+
+    getchar();
+
+    puts("Expressao que sera traduzida:");
+    printf("%s\n", aux);
+
+    puts("Traducao da Expressao:");
+    printf("%s\n", inf);
 
     char teste[10];
     return 0;
